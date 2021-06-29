f7a5ddfc616d6fc72e230ee2ee7b1d263ed4f5de
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-06-29 06:19:16 +0200

    added features to upload page
    added form field , take input of the user name
    added the same features in the verify page

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,14 +1,35 @@
-import React from "react";
-import Card from "../components/UI/Card.js"
-import Form from "../components/Form.js"
+import React, { useState } from "react";
+import Card from "../components/UI/Card.js";
+import CreateForm from "../components/createProf/CreateForm";
+import SuccessIndicator from "react-success-indicator";
 const UploadPage = () => {
+  const [isDone, setIsDone] = useState(false);
+
+  const addSignatureHandler = (signData) => {
+    fetch("RestApiHere", {
+      mode: "no-cors",
+      method: "POST",
+      body: JSON.stringify(signData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setIsDone(true);
+  };
+  if (isDone) {
+    return (
+      <div>
+        <p>
+          <SuccessIndicator size="150px" color="black" />
+        </p>
+        <h3>Profile created successfully!</h3>
+      </div>
+    );
+  }
   return (
     <Card>
-
-      <h1>You Can Upload Content Here</h1>
+      <CreateForm onAdd={addSignatureHandler} />
     </Card>
-     
-
   );
 };
 export default UploadPage;
