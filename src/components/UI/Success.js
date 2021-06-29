f2077243945709e0d28dca706ae40ae2477c62b1
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-06-29 06:22:35 +0200

    Delete Success.js

--- src/components/UI/Success.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SuccessIndicator from "react-success-indicator";
-const SuccessIndicator = () => {
-  return <SuccessIndicator size="96px" color="black" />;
-};
-export default SuccessIndicator;
