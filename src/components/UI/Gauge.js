38d0eeb1e596541de1ec8f0fe13ace41a208f1cf
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-06-29 06:19:44 +0200

    Add files via upload

---
+++ src/components/UI/Gauge.js
@@ -0,0 +1,29 @@
+import React from "react";
+import GaugeChart from "react-gauge-chart";
+import Card from "./Card";
+import styles from "./Gauage.module.css";
+const Gauge = (props) => {
+  const THRESHOLD = 0.7;
+  let message = "The Signature is ";
+  let percentage = props.percentage;
+  if (percentage >= THRESHOLD) {
+    message += "Original";
+  } else {
+    message += "Forged";
+  }
+  return (
+    <div className={styles.control}>
+      <Card>
+        <h2>{message}</h2>
+        <GaugeChart
+          id="gauge-chart2"
+          nrOfLevels={3}
+          percent={percentage}
+          textColor={"#000000"}
+          colors={["#FF0000", "#FFFF00", "#00FF00"]}
+        />
+      </Card>
+    </div>
+  );
+};
+export default Gauge;
---
+++ src/components/UI/Success.js
@@ -0,0 +1,6 @@
+import React from "react";
+import SuccessIndicator from "react-success-indicator";
+const SuccessIndicator = () => {
+  return <SuccessIndicator size="96px" color="black" />;
+};
+export default SuccessIndicator;
