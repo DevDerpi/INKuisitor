2afd95db7f718a72b7a90f8fd76ed998b8b0cb28
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-06-29 06:18:35 +0200

    added new features ot the verify page.
    added selecting a profile option
    added signature pad to allow user to sign on the run
    added feature to allow user to upload his signature

--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -1,6 +1,26 @@
-import React from "react";
-import Form from "../components/Form.js"
+import React, { useState } from "react";
+import Form from "../components/Form.js";
+import Gauge from "../components/UI/Gauge";
 const VerifyPage = () => {
-  return   <Form/>;
+  const [isVerified, setIsVerified] = useState(false);
+  const verifySignatureHandler = (signData) => {
+    fetch("RestApiHere", {
+      mode: "no-cors",
+      method: "POST",
+      body: JSON.stringify(signData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setIsVerified(true);
+  };
+  if (isVerified) {
+    return <Gauge percentage={0.95}/>;
+  }
+  return (
+    <section>
+      <Form onVerify={verifySignatureHandler} />
+    </section>
+  );
 };
 export default VerifyPage;
