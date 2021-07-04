7c0b46b673d86d2f251c613f552b7eae2584730b
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-07-04 19:35:33 +0200

    Add files via upload

--- src/components/createProf/CreateForm.js
+++ src/components/createProf/CreateForm.js
@@ -7,9 +7,7 @@ import Select from "react-select";
 const Form = (props) => {
   const clientNameInputRef = useRef();
   const [selectedOption, setSelectedOption] = useState([]);
-
   const [isDisabled, setIsDisabled] = useState(false);
-  // const userProfileRef = useRef();
   const sigPadRef = useRef({});
   const [trimmedDataUrl, setTrimmedDataUrl] = useState(null);
   const [selectedImage1, setSelectedImage1] = useState(null);
@@ -20,67 +18,30 @@ const Form = (props) => {
   const [signatureData2, setSignatureData2] = useState("");
   const [signatureData3, setSignatureData3] = useState("");
   const [optionID, setOptionID] = useState("");
+  const [flag, setFlag] = useState(0);
   const options = [
     { value: "write", label: "Sign Here" },
     { value: "upload", label: "Upload Your Signature" },
   ];
-  // const blobCreationFromURL = (inputURI) => {
-  //   let binaryVal;
-
-  //   // mime extension extraction
-  //   let inputMIME = inputURI.split(",")[0].split(":")[1].split(";")[0];
-
-  //   // Extract remaining part of URL and convert it to binary value
-  //   if (inputURI.split(",")[0].indexOf("base64") >= 0)
-  //     binaryVal = atob(inputURI.split(",")[1]);
-  //   // Decoding of base64 encoded string
-  //   else binaryVal = unescape(inputURI.split(",")[1]);
-
-  //   // Computation of new string in which hexadecimal
-  //   // escape sequences are replaced by the character
-  //   // it represents
-
-  //   // Store the bytes of the string to a typed array
-  //   let blobArray = [];
-  //   for (let index = 0; index < binaryVal.length; index++) {
-  //     blobArray.push(binaryVal.charCodeAt(index));
-  //   }
-
-  //   return new Blob([blobArray], {
-  //     type: inputMIME,
-  //   });
-  // };
   const inputChangeHandler = () => {
     setEnteredName(clientNameInputRef.current.value);
   };
   //handling submission
   const submitHandler = (event) => {
     event.preventDefault();
-    // const newProfile = {
-    //   clientName: enteredName,
-    //   signatureData,
-    //   optionID,
-    // };
     let formData = new FormData();
     formData.append("clientName", enteredName);
     formData.append("img1", signatureData1);
     formData.append("img2", signatureData2);
     formData.append("img3", signatureData3);
-    //   formData.append('image', {
-    //     uri: signatureData.uri,
-    //     name: signatureData.uri.split('/').pop(), //split the uri at / and get the last element of the resulting array which actually is the name with the image extention (e.g, abc.jpg)
-    //    type: image.type // type needs to be modified. keep reading
-    // })
     formData.append("option", optionID);
-    formData.append("action", "add-signature");
     props.onAdd(formData);
-    // userProfileRef.current.value = "Ahmed";
   };
   const fileChangedHandler = (event) => {
     setSelectedImage1(event.target.files[0]);
     setSelectedImage2(event.target.files[1]);
     setSelectedImage3(event.target.files[2]);
-    setOptionID("P");
+    setOptionID("Image");
     setIsDisabled(true);
   };
   const trim = (event) => {
@@ -88,9 +49,13 @@ const Form = (props) => {
     setTrimmedDataUrl(
       sigPadRef.current.getTrimmedCanvas().toDataURL("image/png")
     );
-    setOptionID("B");
+    let flagCount = flag;
+    setFlag(flagCount + 1);
+    setOptionID("Base64");
     sigPadRef.current.clear();
-    setIsDisabled(true);
+    if (flag === 2) {
+      setIsDisabled(true);
+    }
   };
   useEffect(() => {
     if (selectedImage1 && selectedImage2 && selectedImage3) {
@@ -98,12 +63,15 @@ const Form = (props) => {
       setSignatureData2(selectedImage2);
       setSignatureData3(selectedImage3);
     } else if (trimmedDataUrl) {
-      // let blobObject = blobCreationFromURL(trimmedDataUrl);
-      // setSignatureData(blobObject);
-      setSignatureData1(trimmedDataUrl);
+      if (flag === 1) {
+        setSignatureData1(trimmedDataUrl);
+      } else if (flag === 2) {
+        setSignatureData2(trimmedDataUrl);
+      } else if (flag === 3) {
+        setSignatureData3(trimmedDataUrl);
+      }
     }
-  }, [trimmedDataUrl, selectedImage1,selectedImage2,selectedImage3]);
-  // console.log(signatureData);
+  }, [trimmedDataUrl, selectedImage1, selectedImage2, selectedImage3, flag]);
   return (
     <Card>
       <form className={styles.form} onSubmit={submitHandler}>
@@ -164,4 +132,3 @@ const Form = (props) => {
   );
 };
 export default Form;
-// then inside of our submit handler we can extract user input by storing inside yet another constant that should refer to what we ar extracting
