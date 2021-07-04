8087a2adf1931946db9154eea668b2e149ca86cd
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-07-04 18:18:42 +0200

    made it more suitable for backend Engineer to recieve data from it

--- src/components/createProf/CreateForm.js
+++ src/components/createProf/CreateForm.js
@@ -12,15 +12,44 @@ const Form = (props) => {
   // const userProfileRef = useRef();
   const sigPadRef = useRef({});
   const [trimmedDataUrl, setTrimmedDataUrl] = useState(null);
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage1, setSelectedImage1] = useState(null);
+  const [selectedImage2, setSelectedImage2] = useState(null);
+  const [selectedImage3, setSelectedImage3] = useState(null);
   const [enteredName, setEnteredName] = useState("");
-  const [signatureData, setSignatureData] = useState("");
+  const [signatureData1, setSignatureData1] = useState("");
+  const [signatureData2, setSignatureData2] = useState("");
+  const [signatureData3, setSignatureData3] = useState("");
   const [optionID, setOptionID] = useState("");
   const options = [
     { value: "write", label: "Sign Here" },
     { value: "upload", label: "Upload Your Signature" },
   ];
+  // const blobCreationFromURL = (inputURI) => {
+  //   let binaryVal;
 
+  //   // mime extension extraction
+  //   let inputMIME = inputURI.split(",")[0].split(":")[1].split(";")[0];
+
+  //   // Extract remaining part of URL and convert it to binary value
+  //   if (inputURI.split(",")[0].indexOf("base64") >= 0)
+  //     binaryVal = atob(inputURI.split(",")[1]);
+  //   // Decoding of base64 encoded string
+  //   else binaryVal = unescape(inputURI.split(",")[1]);
+
+  //   // Computation of new string in which hexadecimal
+  //   // escape sequences are replaced by the character
+  //   // it represents
+
+  //   // Store the bytes of the string to a typed array
+  //   let blobArray = [];
+  //   for (let index = 0; index < binaryVal.length; index++) {
+  //     blobArray.push(binaryVal.charCodeAt(index));
+  //   }
+
+  //   return new Blob([blobArray], {
+  //     type: inputMIME,
+  //   });
+  // };
   const inputChangeHandler = () => {
     setEnteredName(clientNameInputRef.current.value);
   };
@@ -33,15 +62,24 @@ const Form = (props) => {
     //   optionID,
     // };
     let formData = new FormData();
-    formData.append("name", enteredName);
-    formData.append("image", signatureData);
+    formData.append("clientName", enteredName);
+    formData.append("img1", signatureData1);
+    formData.append("img2", signatureData2);
+    formData.append("img3", signatureData3);
+    //   formData.append('image', {
+    //     uri: signatureData.uri,
+    //     name: signatureData.uri.split('/').pop(), //split the uri at / and get the last element of the resulting array which actually is the name with the image extention (e.g, abc.jpg)
+    //    type: image.type // type needs to be modified. keep reading
+    // })
     formData.append("option", optionID);
     formData.append("action", "add-signature");
     props.onAdd(formData);
     // userProfileRef.current.value = "Ahmed";
   };
   const fileChangedHandler = (event) => {
-    setSelectedImage(event.target.files);
+    setSelectedImage1(event.target.files[0]);
+    setSelectedImage2(event.target.files[1]);
+    setSelectedImage3(event.target.files[2]);
     setOptionID("P");
     setIsDisabled(true);
   };
@@ -55,12 +93,16 @@ const Form = (props) => {
     setIsDisabled(true);
   };
   useEffect(() => {
-    if (selectedImage) {
-      setSignatureData(selectedImage);
+    if (selectedImage1 && selectedImage2 && selectedImage3) {
+      setSignatureData1(selectedImage1);
+      setSignatureData2(selectedImage2);
+      setSignatureData3(selectedImage3);
     } else if (trimmedDataUrl) {
-      setSignatureData(trimmedDataUrl);
+      // let blobObject = blobCreationFromURL(trimmedDataUrl);
+      // setSignatureData(blobObject);
+      setSignatureData1(trimmedDataUrl);
     }
-  }, [trimmedDataUrl, selectedImage]);
+  }, [trimmedDataUrl, selectedImage1,selectedImage2,selectedImage3]);
   // console.log(signatureData);
   return (
     <Card>
@@ -97,7 +139,11 @@ const Form = (props) => {
               </button>
               <div className={styles.sigImage}>
                 {trimmedDataUrl ? (
-                  <img className={styles.sigImage} src={trimmedDataUrl} alt="" />
+                  <img
+                    className={styles.sigImage}
+                    src={trimmedDataUrl}
+                    alt=""
+                  />
                 ) : null}
               </div>
             </Card>
